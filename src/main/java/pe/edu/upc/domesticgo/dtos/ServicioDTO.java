8fd5b4084f59859de98ac8534c6ed23a3eb6af86
Dragonsupremo01 <118416148+Dragonsupremo01@users.noreply.github.com>
2025-10-05 00:28:57 -0500

    feat: Implementar servicios de reportería para contratos y clientes
    Implementación de la nueva capa de servicios para métricas de contratos.
    Se agregan los métodos 'obtenerServiciosPorMes' y 'obtenerClientesRecurrentes' en ContratoService, junto con la creación de sus DTOs correspondientes (ServiciosContratadosDTO y ClientesRecurrentesDTO).

---
+++ src/main/java/pe/edu/upc/domesticgo/dtos/ServicioDTO.java
@@ -0,0 +1,63 @@
+package pe.edu.upc.domesticgo.dtos;
+
+import pe.edu.upc.domesticgo.entities.Contrato;
+import pe.edu.upc.domesticgo.entities.Reserva;
+
+public class ServicioDTO {
+    private int idServicio;
+    private String estadoServicio;
+    private String tipoServicio;
+    private Contrato contrato;
+    private Reserva reserva;
+
+    public ServicioDTO(int idServicio, String estadoServicio, String tipoServicio, Contrato contrato, Reserva reserva) {
+        this.idServicio = idServicio;
+        this.estadoServicio = estadoServicio;
+        this.tipoServicio = tipoServicio;
+        this.contrato = contrato;
+        this.reserva = reserva;
+    }
+
+    // Getters and Setters
+    public int getIdServicio() {
+        return idServicio;
+    }
+
+    public void setIdServicio(int idServicio) {
+        this.idServicio = idServicio;
+    }
+
+    public String getEstadoServicio() {
+        return estadoServicio;
+    }
+
+    public void setEstadoServicio(String estadoServicio) {
+        this.estadoServicio = estadoServicio;
+    }
+
+    public String getTipoServicio() {
+        return tipoServicio;
+    }
+
+    public void setTipoServicio(String tipoServicio) {
+        this.tipoServicio = tipoServicio;
+    }
+
+    public Contrato getContrato() {
+        return contrato;
+    }
+
+    public void setContrato(Contrato contrato) {
+        this.contrato = contrato;
+    }
+
+    public Reserva getReserva() {
+        return reserva;
+    }
+    public void setReserva(Reserva reserva) {
+        this.reserva = reserva;
+    }
+
+
+
+}
