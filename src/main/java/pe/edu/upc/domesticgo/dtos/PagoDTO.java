58973323d0e810deb9fa0d4c290db20ea603134b
MF <145232992+MartinFer1602@users.noreply.github.com>
2025-10-05 01:11:10 -0500

    DTO
    Notificacion
    NotificacionesPorUsuario
    Pago
    PromedioResenia

---
+++ src/main/java/pe/edu/upc/domesticgo/dtos/PagoDTO.java
@@ -0,0 +1,82 @@
+package pe.edu.upc.domesticgo.dtos;
+
+import pe.edu.upc.domesticgo.entities.Contrato;
+
+import java.time.LocalDate;
+
+public class PagoDTO {
+    private int idPago;
+    private String tipoPago;
+    private String tipoComprobante;
+    private String bancoPago;
+    private LocalDate fechaPago;
+    private double montoPago;
+    private String estadoPago;
+    private Contrato contrato;
+
+    // Getters and Setters
+
+    public int getIdPago() {
+        return idPago;
+    }
+
+    public void setIdPago(int idPago) {
+        this.idPago = idPago;
+    }
+
+    public String getTipoPago() {
+        return tipoPago;
+    }
+
+    public void setTipoPago(String tipoPago) {
+        this.tipoPago = tipoPago;
+    }
+
+    public String getTipoComprobante() {
+        return tipoComprobante;
+    }
+
+    public void setTipoComprobante(String tipoComprobante) {
+        this.tipoComprobante = tipoComprobante;
+    }
+
+    public String getBancoPago() {
+        return bancoPago;
+    }
+
+    public void setBancoPago(String bancoPago) {
+        this.bancoPago = bancoPago;
+    }
+
+    public LocalDate getFechaPago() {
+        return fechaPago;
+    }
+
+    public void setFechaPago(LocalDate fechaPago) {
+        this.fechaPago = fechaPago;
+    }
+
+    public double getMontoPago() {
+        return montoPago;
+    }
+
+    public void setMontoPago(double montoPago) {
+        this.montoPago = montoPago;
+    }
+
+    public String getEstadoPago() {
+        return estadoPago;
+    }
+
+    public void setEstadoPago(String estadoPago) {
+        this.estadoPago = estadoPago;
+    }
+
+    public Contrato getContrato() {
+        return contrato;
+    }
+
+    public void setContrato(Contrato contrato) {
+        this.contrato = contrato;
+    }
+}
